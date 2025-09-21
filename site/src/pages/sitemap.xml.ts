179e9130942ea9cf1df9e7990abb5bf3d2b5a2d3
Patrick Parker <patrick.parker@empowerid.com>
2025-09-21 16:42:55 -0400

    site: cross-site docs nav; add comparisons/industries sitemaps; sitemap index updated

--- site/src/pages/sitemap.xml.ts
+++ site/src/pages/sitemap.xml.ts
@@ -6,6 +6,8 @@ export async function GET({ request }: APIContext) {
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	<sitemap><loc>${origin}/sitemaps/products.xml</loc></sitemap>
 	<sitemap><loc>${origin}/sitemaps/solutions.xml</loc></sitemap>
+	<sitemap><loc>${origin}/sitemaps/comparisons.xml</loc></sitemap>
+	<sitemap><loc>${origin}/sitemaps/industries.xml</loc></sitemap>
 </sitemapindex>`;
 	return new Response(xml, { headers: { 'Content-Type': 'application/xml' } });
 }
