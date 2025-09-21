addf6b737dfb4264576adee0dfe1abac6aff3e44
Patrick Parker <patrick.parker@empowerid.com>
2025-09-21 15:51:56 -0400

    site: add Base/Product/Solution/Primer layouts with NeonFlux theme; wire index page

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://empowerid.github.io/empowernow_info',
+	integrations: [mdx(), sitemap()],
+});
